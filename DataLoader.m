072187c0978d9178c3bad6380603e5ea0042a32f
lukarson <48386569+lukarson@users.noreply.github.com>
2019-06-08 16:34:08 +0200

    add dataloader script

---
+++ DataLoader.m
@@ -0,0 +1,24 @@
+
+clear;
+format compact;
+nntwarn off;
+load winequality-red.data; % Domyœlnie ³adowany zbiór jakoœci wina
+
+P = winequality_red(:,1:11)'; % P i T dopasowane do danych WineQuality
+T = winequality_red(:,12)';   % Rozmiary dla innego zbioru bêd¹ siê ró¿niæ
+
+% [ [1:11]; min(P'); max(P') ]';
+
+Pn = mapminmax(P);
+% plot(T);
+% plot(sort(T));
+
+[Ts, ind_Ts] = sort(T);
+Pns = zeros(size(Pn));
+
+for i = 1:length(ind_Ts)
+    Pns(:,i) = Pn(:, ind_Ts(i));
+end
+
+    
+    
